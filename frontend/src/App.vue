bfe4d502c05da5a6f18382e64cd10227b39981ce
Reiniel Dan Pablo <reinieldan@gmail.com>
2021-10-18 11:24:54 +0800

    :+1: Moved files -> /frontend; Edited some files to test tailwind

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/> -->
+  <div id="start-cover" class="bg-gray-500">
+    Welcome to my site.
+  </div>
+</template>
+
+<style>
+</style>
